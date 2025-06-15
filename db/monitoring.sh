099c9defc537895fb804f6c69f5ba136c77873c5
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-15 18:51:46 +0530

    work on installing monitoring

---
+++ db/monitoring.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "installing prometheus"
+
+echo " create namespace for monitoring"
+
+kubectl create namespace monitoring
+kubectl apply -f configmap.yaml
+kubectl apply -f prom-deployment.yaml
+kubectl apply -f prom-service.yaml
+kubectl apply -f grafana-deployment.yaml
+kubectl apply -f grafana-service.yaml
