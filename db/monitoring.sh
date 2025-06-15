72cf2c5ac3156f3e9b95451a0b9354606f14acb0
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-15 20:23:49 +0530

    work on installing monitoring

--- db/monitoring.sh
+++ db/monitoring.sh
@@ -5,7 +5,7 @@ echo "installing prometheus"
 echo " create namespace for monitoring"
 
 kubectl create namespace monitoring
-kubectl apply -f configmap.yaml
+kubectl apply -f configmap.yaml -n monitoring
 kubectl apply -f prom-deployment.yaml
 kubectl apply -f prom-service.yaml
 kubectl apply -f grafana-deployment.yaml
