63651194c77b4f56440f6bc417b9b22adad44644
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-15 16:06:47 +0530

    updated AMI

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_type" {
 
 variable "ami_id" {
   description = "Ubuntu 22.04 AMI for ap-south-1"
-  default     = "ami-0f58b397bc5c1f2e8"
+  default     = "ami-09e6f87a47903347c"
 }
 
 variable "public_key_path" {
