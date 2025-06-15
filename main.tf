740186a2f70800e13d9121e300dfd3057593cbda
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-15 20:31:10 +0530

    security group port changes

--- main.tf
+++ main.tf
@@ -21,6 +21,13 @@ resource "aws_security_group" "kind_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress = {
+    from_port = 30030
+    to_port   = 30090
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
