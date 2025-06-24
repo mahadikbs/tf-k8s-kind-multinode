fb215de77a12cb437d9b19843ca5f0f7c1746588
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-24 11:09:19 +0530

    add jenkins installation

---
+++ db/jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+kubectl create namespace jenkins
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+
+helm install jenkins jenkins/jenkins \
+  --namespace jenkins \
+  --set controller.serviceType=LoadBalancer
+
+echo "This is an initial admin password for jenkins"
+
+kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
