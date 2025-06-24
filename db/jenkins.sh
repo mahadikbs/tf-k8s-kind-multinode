761cbde40048f5060de1062454f47d5f32c18a75
Brahmadev Shivaji Mahadik <brahmadev.mahadik@yash.com>
2025-06-24 12:55:27 +0530

    update kind conf

--- db/jenkins.sh
+++ db/jenkins.sh
@@ -3,11 +3,5 @@ kubectl create namespace jenkins
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
 
-helm install jenkins jenkins/jenkins \
-  --namespace jenkins \
-  --set controller.serviceType=NodePort \
-  --set controller.service.nodePort=31492
-
-echo "This is an initial admin password for jenkins"
-
-kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
+helm install jenkins jenkins/jenkins --namespace jenkins -f values.yaml
+  
